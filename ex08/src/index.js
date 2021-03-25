2fcdddced73b04fc74ecbe7a517d483e76bd0b42
Kserhaja <kenanserhatlic@hotmail.com>
2021-03-25 10:10:09 +0100

    lodash 01

---
+++ ex08/src/index.js
@@ -0,0 +1,23 @@
+const _ = requiere('lodash');
+// Only change code below this line
+// user nested array with four objects star here
+
+
+// users nested array with four object ends  here
+
+// getUsers function - list of users starts here
+
+
+
+
+// getUsers function - list of users ends here 
+
+
+// findUser(lastName, gender) function starts here
+const person = [firstName = "John", lastName = "Doe", age = 24, gender = "male"];
+
+function getUsers()
+
+
+
+
